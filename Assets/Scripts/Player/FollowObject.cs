54c898ae882021f03bba6951098267b5cae058b5
CakuDev <cakudev@gmail.com>
2024-11-30 21:16:43 +0100

    Add visual elements to scenes

---
+++ Assets/Scripts/Player/FollowObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObject : MonoBehaviour
+{
+    [SerializeField] private bool moveOnX = false;
+    [SerializeField] private bool moveOnY = false;
+    [SerializeField] private bool moveOnZ = false;
+    [SerializeField] private Transform objectToFollow;
+    [SerializeField] private Vector3 offset;
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+        if (moveOnX) position.x = objectToFollow.position.x + offset.x;
+        if (moveOnY) position.y = objectToFollow.position.y + offset.y;
+        if (moveOnZ) position.z = objectToFollow.position.z + offset.z;
+        transform.position = position;
+    }
+}
